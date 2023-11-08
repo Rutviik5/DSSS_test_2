4c70158ef53cfed46f57badb5fad47ee7b495e34
Rutvik Dudhat <prutvik875@gmail.com>
2023-11-08 12:55:32 +0100

    added setup.py file

---
+++ math_quiz/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-project',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.18.5',
+        'pandas==1.0.5',
+    ],
+    author='Rutvik Dudhat',
+    author_email='prutvik875@email.com',
+    description='DSSS maths homework 2',
+)
