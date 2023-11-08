50c0b1d539a03e4de66c95a747fadefe81c4d7d1
RUTVIK DUDHAT <148854003+Rutviik5@users.noreply.github.com>
2023-11-08 22:40:23 +0100

    Update setup.py
    Deleted

--- setup.py
+++ setup.py
@@ -1,14 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name='my_math_HW',
-    version='1.0.0',
-    packages=find_packages(),
-    install_requires=[
-        'numpy==1.18.5',
-        'pandas==1.0.5',
-    ],
-    author='Rutvik Dudhat',
-    author_email='prutvik875@email.com',
-    description='DSSS maths homework 2',
-)
